c2928e1283aff9c328a5fe211d678ccaf8d86b2b
Roman Efimov <romaonthego@gmail.com>
2013-01-26 22:30:43 -0600

    VKontakte sharing

--- REActivityViewController/REVKActivity.m
+++ REActivityViewController/REVKActivity.m
@@ -9,6 +9,7 @@
 #import "REVKActivity.h"
 #import "REActivityViewController.h"
 #import "REVKActivityViewController.h"
+#import "AFNetworking.h"
 
 @implementation REVKActivity
 
@@ -46,22 +47,154 @@
     NSDictionary *userInfo = self.activityViewController.userInfo;
 
     NSString *text = [userInfo objectForKey:@"text"];
+    NSURL *url = [userInfo objectForKey:@"url"];
     UIImage *image = [userInfo objectForKey:@"image"];
+    
+    NSString *textToShare;
+    if (text && !url)
+        textToShare = text;
+    
+    if (!text && url)
+        textToShare = url.absoluteString;
+    
+    if (text && url)
+        textToShare = [NSString stringWithFormat:@"%@ %@", text, url.absoluteString];
     
     REComposeViewController *controller = [[REComposeViewController alloc] init];
     controller.title = @"VKontakte";
     controller.navigationBar.tintColor = [UIColor colorWithRed:56/255.0f green:99/255.0f blue:150/255.0f alpha:1.0];
-    if (text)
-        controller.text = text;
+    if (textToShare)
+        controller.text = textToShare;
     if (image) {
         controller.hasAttachment = YES;
         controller.attachmentImage = image;
     }
     controller.completionHandler = ^(REComposeResult result){
         presenter.modalPresentationStyle = UIModalPresentationFullScreen;
+        
+        if (result == REComposeResultPosted) {
+            if (image) {
+                [self shareText:controller.text image:image];
+            } else {
+                [self shareText:controller.text];
+            }
+        }
     };
     presenter.modalPresentationStyle = UIModalPresentationCurrentContext;
     [presenter presentViewController:controller animated:YES completion:nil];
+}
+
+#pragma mark -
+#pragma mark VK sharing
+
+- (void)requestPhotoUploadURLWithSuccess:(void (^)(NSString *uploadURL))success
+{
+    NSString *serverURL = [NSString stringWithFormat:@"https://api.vk.com/method/photos.getWallUploadServer?owner_id=%@&access_token=%@", self.ownerId, self.token];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:serverURL]];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        if (success) {
+            success([[JSON objectForKey:@"response"] objectForKey:@"upload_url"]);
+        }
+    } failure:nil];
+    [operation start];
+}
+
+- (void)uploadImage:(UIImage *)image toURL:(NSString *)urlString success:(void (^)(NSString *hash, NSString *photo, NSString *server))success
+{
+    NSURL *url = [NSURL URLWithString:urlString];
+    AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:url];
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.75f);
+    NSMutableURLRequest *request = [httpClient multipartFormRequestWithMethod:@"POST" path:@"" parameters:nil
+                                                    constructingBodyWithBlock:^(id <AFMultipartFormData>formData) {
+        [formData appendPartWithFileData:imageData name:@"photo" fileName:@"photo.jpg" mimeType:@"image/jpg"];
+    }];
+    
+    void (^parseJSON)(id JSON) = ^(id JSON){
+        if (success)
+            success([JSON objectForKey:@"hash"], [JSON objectForKey:@"photo"], [JSON objectForKey:@"server"]);
+    };
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        parseJSON(JSON);
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        parseJSON(JSON);
+    }];
+    [operation start];
+}
+
+- (void)saveImageToWallWithHash:(NSString *)hash photo:(NSString *)photo server:(NSString *)server success:(void (^)(NSString *wallPhotoId))success
+{
+    NSString *serverURL = [NSString stringWithFormat:@"https://api.vk.com/method/photos.saveWallPhoto?owner_id=%@&access_token=%@&server=%@&photo=%@&hash=%@", self.ownerId, self.token, server, photo, hash];
+    NSString *escapedURL = [serverURL stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:escapedURL]];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        if (success)
+            success([[[JSON objectForKey:@"response"] objectAtIndex:0] objectForKey:@"id"]);
+    } failure:nil];
+    [operation start];
+}
+
+- (void)shareOnWall:(NSString *)text photoId:(NSString *)wallPhotoId completion:(void (^)(void))completion
+{
+    NSString *serverURL;
+    
+    if (wallPhotoId) {
+        serverURL = [NSString stringWithFormat:@"https://api.vk.com/method/wall.post?owner_id=%@&access_token=%@&message=%@&attachment=%@", self.ownerId, self.token, [self URLEncodedString:text], wallPhotoId];
+    } else {
+        serverURL = [NSString stringWithFormat:@"https://api.vk.com/method/wall.post?owner_id=%@&access_token=%@&message=%@", self.ownerId, self.token, [self URLEncodedString:text]];
+    }
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:serverURL]];
+        
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        if (completion)
+            completion();
+    } failure:nil];
+    [operation start];
+}
+
+- (void)shareText:(NSString *)text image:(UIImage *)image
+{
+    [self requestPhotoUploadURLWithSuccess:^(NSString *uploadURL) {
+        [self uploadImage:image toURL:uploadURL success:^(NSString *hash, NSString *photo, NSString *server) {
+            [self saveImageToWallWithHash:hash photo:photo server:server success:^(NSString *wallPhotoId) {
+                [self shareOnWall:text photoId:wallPhotoId completion:nil];
+            }];
+        }];
+    }];
+}
+
+- (void)shareText:(NSString *)text
+{
+    [self shareOnWall:text photoId:nil completion:nil];
+}
+
+#pragma mark -
+#pragma Helper
+
+- (NSString *)URLEncodedString:(NSString *)str
+{
+    NSString *result = (__bridge NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+                                                                                    (CFStringRef)str,
+                                                                                    NULL,
+                                                                                    CFSTR("!*'();:@&=+$,/?%#[]"),
+                                                                                    kCFStringEncodingUTF8);
+	return result;
+}
+
+#pragma mark -
+#pragma mark Credentials
+
+- (NSString *)token
+{
+    return [[NSUserDefaults standardUserDefaults] objectForKey:@"REVKActivity_Token"];
+}
+
+- (NSString *)ownerId
+{
+    return [[NSUserDefaults standardUserDefaults] objectForKey:@"REVKActivity_UserId"];
 }
 
 @end
