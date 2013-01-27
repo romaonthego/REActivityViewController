e5f6800ac4564e98dabd05ee6b8ba03d4ee56bd3
Roman Efimov <romaonthego@gmail.com>
2013-01-27 13:13:09 -0600

    Instapaper

--- REActivityViewController/REInstapaperActivity.m
+++ REActivityViewController/REInstapaperActivity.m
@@ -8,16 +8,29 @@
 
 #import "REInstapaperActivity.h"
 #import "REActivityViewController.h"
+#import "AFNetworking.h"
 
 @implementation REInstapaperActivity
 
 - (id)init
 {
-    return [super initWithTitle:@"Save to Instapaper"
+    return [super initWithTitle:@"Send to Instapaper"
                           image:[UIImage imageNamed:@"Icon_Instapaper"]
                     actionBlock:^(REActivity *activity, REActivityViewController *activityViewController) {
-                      // UIViewController *presenter = activityViewController.presentingController;
-                       // NSDictionary *userInfo = activityViewController.userInfo;
+                        NSURL *url = [NSURL URLWithString:@"https://www.instapaper.com/api/add"];
+                        AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:url];
+                
+                        [httpClient setAuthorizationHeaderWithUsername:@"romefimov@gmail.com" password:@""];
+                        NSDictionary *params = @{
+                            @"title": @"TEXT 123",
+                            @"url": @"https://github.com/romaonthego"
+                        };
+                        [httpClient postPath:@"" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                            NSString *responseStr = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+                            NSLog(@"Request Successful, response '%@'", responseStr);
+                        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                            NSLog(@"[HTTPClient Error]: %@", error.localizedDescription);
+                        }];
                     }];
 }
 
