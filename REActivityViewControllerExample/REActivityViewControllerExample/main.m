7152e4e387c1d4ebe69e92d0a7ee284a14b94a33
Roman Efimov <romaonthego@gmail.com>
2013-01-31 10:38:31 -0600

    Add clean sample project

---
+++ REActivityViewControllerExample/REActivityViewControllerExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  REActivityViewControllerExample
+//
+//  Created by Roman Efimov on 1/31/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
